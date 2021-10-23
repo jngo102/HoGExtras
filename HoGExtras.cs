e9d771bfb352d8ba84dde68f45e9f3b66e21bc7c
jngo102 <jngo102@aim.com>
2021-10-23 01:11:19 -0400

    Fixed focus breaking after defeating THK, adjusted radiance victory to appear more like absrad's

--- HoGExtras.cs
+++ HoGExtras.cs
@@ -1,14 +1,14 @@
-﻿using HutongGames.PlayMaker.Actions;
+﻿using HutongGames.PlayMaker;
+using HutongGames.PlayMaker.Actions;
 using Modding;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Vasi;
 using UObject = UnityEngine.Object;
 using USceneMgr = UnityEngine.SceneManagement.SceneManager;
-using Vasi;
-using HutongGames.PlayMaker;
-using System.Collections;
 
 namespace HoGExtras
 {
@@ -28,8 +28,8 @@ namespace HoGExtras
         {
             return new List<ValueTuple<string, string>>
             {
-                new ValueTuple<string, string>("Room_Final_Boss_Core", "Boss Control"),
-                new ValueTuple<string, string>("Dream_Final_Boss", "Boss Control"),
+                ("Room_Final_Boss_Core", "Boss Control"),
+                ("Dream_Final_Boss", "Boss Control"),
             };
         }
 
@@ -42,7 +42,6 @@ namespace HoGExtras
         {
             _preloads.Add("THK", preloadedObjects["Room_Final_Boss_Core"]["Boss Control"]);
             _preloads.Add("Rad", preloadedObjects["Dream_Final_Boss"]["Boss Control"]);
-
 
             ModHooks.LanguageGetHook += OnLangGet;
             ModHooks.GetPlayerVariableHook += GetVarHook;
@@ -103,6 +102,7 @@ namespace HoGExtras
                 case "GG_Workshop":
                     SetStatue("HollowKnight", "NAME_THK", "GG_S_THK", "statueStateTHK", "statueStateHollowKnight", 2);
                     SetStatue("Radiance", "NAME_RAD", "GG_S_RAD", "statueStateRad", "statueStateRadiance", 0);
+
                     break;
                 case "GG_Hollow_Knight":
                     if (_localSettings.StatueStateHollowKnight.usingAltVersion) break;
@@ -129,26 +129,22 @@ namespace HoGExtras
                         phaseCtrl.Fsm.GetFsmInt("Phase3 HP").Value = 460;
                     }
 
-                    FsmUtil.RemoveAction<PlayerDataBoolTest>(control.GetState("Long Roar End"));
-                    FsmUtil.RemoveAction<PlayerDataBoolTest>(phaseCtrl.GetState("Set Phase 4"));
+                    control.GetState("Long Roar End").RemoveAction<PlayerDataBoolTest>();
+                    phaseCtrl.GetState("Set Phase 4").RemoveAction<PlayerDataBoolTest>();
                     GameObject bossCorpse = thk.transform.Find("Boss Corpse").gameObject;
                     PlayMakerFSM corpse = bossCorpse.LocateMyFSM("Corpse");
-                    FsmUtil.RemoveAction<SendEventByName>(corpse.GetState("Burst"));
-                    FsmUtil.AddMethod(corpse.GetState("Blow"), () =>
-                    {
-                        GameCameras.instance.StopCameraShake();
-                        bsc.EndBossScene();
-                    });
+                    corpse.GetState("Burst").RemoveAction<SendEventByName>();
+                    corpse.GetState("Blow").AddMethod(() => bsc.EndBossScene());
+                    corpse.GetState("Set Knight Focus").RemoveAction<SetFsmBool>();
 
                     control.SetState("Init");
 
                     var battleScene = GameObject.Find("Battle Scene");
                     GameObject godseeker = battleScene.transform.Find("Godseeker Crowd").gameObject;
                     godseeker.transform.SetParent(null);
-                    var target = godseeker.LocateMyFSM("Control").Fsm.GetFsmGameObject("Target");
+                    FsmGameObject target = godseeker.LocateMyFSM("Control").Fsm.GetFsmGameObject("Target");
                     target.Value = thk;
-                    FsmUtil.AddMethod(battleStart.GetState("Roar Antic"), () => target.Value = HeroController.instance.gameObject);
-
+                    battleStart.GetState("Roar Antic").AddMethod(() => target.Value = HeroController.instance.gameObject);
                     UObject.Destroy(battleScene);
 
                     break;
@@ -157,18 +153,83 @@ namespace HoGExtras
 
                     bossCtrl = UObject.Instantiate(Preloads["Rad"]);
                     bossCtrl.SetActive(true);
-                    control = bossCtrl.transform.Find("Radiance").gameObject.LocateMyFSM("Control");
-                    FsmState tendrils2 = control.GetState("Tendrils 2");
-                    FsmUtil.RemoveTransition(tendrils2, "FINISHED");
-                    FsmUtil.AddMethod(tendrils2, () => BossSceneController.Instance.EndBossScene());
+
+                    Transform rad = bossCtrl.transform.Find("Radiance");
+                    control = rad.gameObject.LocateMyFSM("Control");
+                    FsmState ballTween = control.GetState("Ball Tween");
+                    ballTween.AddCoroutine(RadDeath);
+                    control.CreateState("Dummy");
+                    ballTween.ChangeTransition("FINISHED", "Dummy");
 
                     bossCtrl = GameObject.Find("Boss Control");
+                    PlayMakerFSM arCtrl = bossCtrl.transform.Find("Absolute Radiance").gameObject.LocateMyFSM("Control");
+                    _bossExplode = arCtrl.GetState("Knight Break").GetAction<AudioPlayerOneShotSingle>().audioClip.Value as AudioClip;
+                    _finalHitPt3 = arCtrl.GetState("Statue Death 2").GetAction<AudioPlayerOneShotSingle>().audioClip.Value as AudioClip;
+                    _rumble = arCtrl.GetState("Statue Death 1").GetAction<AudioPlayerOneShotSingle>().audioClip.Value as AudioClip;
                     godseeker = bossCtrl.transform.Find("Godseeker Crowd").gameObject;
                     godseeker.transform.SetParent(null);
                     UObject.Destroy(bossCtrl);
 
                     break;
             }
+        }
+
+        private AudioClip _bossExplode, _finalHitPt3, _rumble;
+        private IEnumerator RadDeath()
+        {
+            Transform bossCtrl = GameObject.Find("Boss Control(Clone)(Clone)").transform;
+            Transform rad = bossCtrl.Find("Radiance");
+
+            Transform knightSplit = rad.Find("Death/Knight Split");
+            var splitAnim = knightSplit.GetComponent<tk2dSpriteAnimator>();
+
+            yield return new WaitForSeconds(splitAnim.PlayAnimGetTime("Knight Split Antic"));
+
+            splitAnim.Play("Knight Split");
+            knightSplit.gameObject.GetComponent<PlayMakerFSM>().SendEvent("SPLIT");
+
+            Transform knightBall = knightSplit.Find("Knight Ball");
+            knightBall.gameObject.SetActive(true);
+            knightBall.SetParent(null);
+            var ballAnim = knightBall.GetComponent<tk2dSpriteAnimator>();
+            ballAnim.Play("Knight Unform");
+
+            Hashtable hashtable = new();
+            hashtable.Add("amount", Vector3.down * 20);
+            hashtable.Add("speed", 10);
+            hashtable.Add("easetype", iTween.EaseType.easeInSine);
+            iTween.MoveBy(ballAnim.gameObject, hashtable);
+
+            GameObject actorPrefab = GameManager.instance.transform.Find("GlobalPool/Audio Player Actor 2D(Clone)").gameObject;
+
+            GameObject audioPlayerActor = actorPrefab.Spawn(HeroController.instance.transform.position);
+            var audioSource = audioPlayerActor.GetComponent<AudioSource>();
+            audioSource.clip = _bossExplode;
+            audioSource.Play();
+
+            yield return new WaitForSeconds(3);
+
+            var ps = rad.Find("Death Pt").GetComponent<ParticleSystem>();
+            ParticleSystem.MainModule main = ps.main;
+            main.loop = true;
+            ParticleSystem.EmissionModule emission = ps.emission;
+            emission.rateOverTime = 500;
+            ps.Play();
+            
+            GameCameras.instance.cameraShakeFSM.SendEvent("HugeShake");
+
+            var heroAudio = HeroController.instance.GetComponent<AudioSource>();
+            heroAudio.clip = _rumble;
+            heroAudio.Play();
+
+            yield return new WaitForSeconds(3);
+
+            audioPlayerActor = actorPrefab.Spawn(HeroController.instance.transform.position);
+            audioSource = audioPlayerActor.GetComponent<AudioSource>();
+            audioSource.clip = _finalHitPt3;
+            audioSource.Play();
+
+            BossSceneController.Instance.EndBossScene();
         }
 
         private void SetStatue(string statueName, string bossName, string bossDesc, string statueStatePD, string dreamStatueStatePD, float switchOffset)
